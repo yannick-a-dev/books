49703040440e6eb63718ba11fc9bd1c63a7cd712
Yannick <essolayk@yahoo.fr>
2021-10-13 14:34:29 +0200

    first

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase'; 
+import { AuthService } from './../services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  
+  isAuth!: boolean;
+  constructor(private AuthService: AuthService ) { }
 
   ngOnInit(): void {
+    firebase.auth().onAuthStateChanged(
+      (user) => {
+        if(user) {
+          this.isAuth = true;
+        } else {
+          this.isAuth = false;
+        }
+      }
+    );
+  }
+
+  onSignOut() {
+    this.AuthService.signOutUser();
   }
 
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Book } from '../models/Book.model';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
   constructor() { }
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/book')
+    .on('value',(data)=> {
+      this.books = data?.val() ? data.val() : [];
+      this.emitBooks();
+    });
+  }
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
 }
